dba7819bbe1e89ed04a2a2c5cc89dbdcb949e1f8
Wesley B <wesleyb@pm.me>
2019-10-01 15:14:28 -0500

    Quick: Add `ReactHelpers.getKey`

---
+++ scripts/react/react-helpers.js
@@ -0,0 +1,17 @@
+/**
+ * Return the most unique **and** stable identifier available
+ * @param {Number|String} id - A unique and reliable ID (ideal)
+ * @param {Array.<Number|String>} [alternatives] - List of entities to concatenate into a key
+ * @param {Number} [index] - The current index of the option in a list
+ * @param {String} [separator="-"] - The character to place between alternatives
+ * @return {String}
+ */
+function getKey( id, alternatives, index, separator = '-' ) {
+	let alternative = alternatives.filter( alt => alt ).join( separator );
+
+	return id || alternative || index;
+}
+
+window.ReactHelpers = {
+	getKey
+};
