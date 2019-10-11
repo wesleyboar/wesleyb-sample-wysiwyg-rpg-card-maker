6319f2621bcff284137206076f538f1c3009626a
Wesley B <wesleyb@pm.me>
2019-10-11 15:18:37 -0500

    Noop: Remove comment and add space

--- src/react/App.js
+++ src/react/App.js
@@ -4,12 +4,9 @@ import defaultValues from '../_shared/default-values.json';
 // Components
 import Card from './components/Card.js';
 
-// Styles
-// import './App.css';
-
 class App extends React.Component {
 	render(){
-		return(
+		return (
 			<Card id="card" {...defaultValues} />
 		);
 	}
