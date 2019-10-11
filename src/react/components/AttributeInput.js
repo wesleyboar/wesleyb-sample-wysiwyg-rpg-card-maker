18a5803c7da82185721a1794b645e18ee95b5733
Wesley B <wesleyb@pm.me>
2019-10-11 15:27:44 -0500

    3: Add/Load `c-attr-input`/`AttributeInput` styles

--- src/react/components/AttributeInput.js
+++ src/react/components/AttributeInput.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Styles
+import './AttributeInput.css';
+
 /** Dynamically-applied CSS class names */
 const CLASSNAMES = {
   neg: 'is-negative',
