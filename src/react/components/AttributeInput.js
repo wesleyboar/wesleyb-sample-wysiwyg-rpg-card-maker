7cd4aa9a7f91850a7ed4808ecad1eea1339cb449
Wesley B <wesleyb@pm.me>
2019-10-11 15:26:15 -0500

    3: Add optional props to `AttributeInput`
    - `tagName`
    - `className`
    - `fieldClassName`

--- src/react/components/AttributeInput.js
+++ src/react/components/AttributeInput.js
@@ -7,6 +7,9 @@ const CLASSNAMES = {
   pos: 'is-positive'
 }
 
+/** The markup tag used if markup is needed but not defined **/
+const defaultTagName = 'div';
+
 /**
  * Called when input/field value changes
  * @callback AttributeInput~onChange
@@ -15,6 +18,8 @@ const CLASSNAMES = {
 
 /**
  * An attribute input field as a range
+ *
+ * _If either `tagName` or `className` is provided, the attribute markup will be wrapped in an element that uses those properties._
  * @param {Object} props
  * @param {String} props.id - Markup `id` attribute for field
  * @param {String} props.label - Field name for humans
@@ -23,6 +28,9 @@ const CLASSNAMES = {
  * @param {Number} props.max - Value maximum
  * @param {Number} [props.step] - Value increment
  * @param {String} [props.value=0] - Field value
+ * @param {String} [props.tagName] - The markup tag in which to wrap the attribute
+ * @param {String} [props.className] - The `className` for the markup tag in which to wrap the attribute
+ * @param {String} [props.fieldClassName] - The `className` for the input field
  * @param {String} [props.labelClassName] - The `className` for the `label`
  * @param {String} [props.outputClassName] - The `className` for the `output`
  * @param {AttributeInput~onChange} [props.onChange] - Callback on value change
@@ -30,9 +38,10 @@ const CLASSNAMES = {
  * @return {React.Component}
  */
 function AttributeInput( props ) {
-	const { id, label, desc, value: initialValue = 0, labelClassName, outputClassName, onChange, ...markupAttrs } = props;
+	const { id, label, desc, value: initialValue = 0, tagName, className, fieldClassName, labelClassName, outputClassName, onChange, ...markupAttrs } = props;
 	const [ value, setValue ] = React.useState( initialValue );
 	const [ signClassName, setSignClassName ] = React.useState('');
+	const TagName = tagName || ( className ) ? defaultTagName : React.Fragment;
 
 	function handleChange( e ) {
 		setValue( e.target.value );
@@ -43,16 +52,17 @@ function AttributeInput( props ) {
 	}, [ value ]);
 
 	return (
-		<React.Fragment>
+		<TagName className={className}>
 			<label htmlFor={id}
 				className={labelClassName}
 				title={desc}>{label}</label>
 			<input id={id}
+				className={fieldClassName}
 				type="range" {...markupAttrs}
 				value={value} onChange={handleChange} />
 			<output htmlFor={id}
 				className={outputClassName + " " + signClassName}>{value}</output>
-		</React.Fragment>
+		</TagName>
 	);
 }
 AttributeInput.propTypes = {
@@ -64,6 +74,9 @@ AttributeInput.propTypes = {
 
 	step: PropTypes.number,
 	value: PropTypes.number,
+	tagName: PropTypes.string,
+	className: PropTypes.string,
+	fieldClassName: PropTypes.string,
 	labelClassName: PropTypes.string,
 	outputClassName: PropTypes.string,
 	onChange: PropTypes.func,
